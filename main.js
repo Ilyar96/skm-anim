99d7fbdcf30a883c04fe1c4e0799adc2bc48f6be
Ilyar <kasyymov.ilyar@gmail.com>
2023-06-15 09:43:01 +0300

    fix

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
 const lerp = (v0, v1, t) => v0 * (1 - t) + v1 * t;
 
-const bgWrapper = document.querySelector(".intro__screen");
-const bgBlock1 = document.querySelector(".bg-block--1");
-const bgBlock2 = document.querySelector(".bg-block--2");
-const bgBlock3 = document.querySelector(".bg-block--3");
-const nodeList1 = [];
-const nodeList2 = [];
-const nodeList3 = [];
+const starsWrapper = document.querySelector(".intro__screen");
+const starsBlock1 = document.querySelector(".stars-block--1");
+const starsBlock2 = document.querySelector(".stars-block--2");
+const starsBlock3 = document.querySelector(".stars-block--3");
 
-const cellWidthRange = { min: 60, max: 90 };
-const cellHeightRange = { min: 60, max: 90 };
+const cellWidthRange = { min: 120, max: 150 };
+const cellHeightRange = { min: 120, max: 150 };
 
 function random(min, max) {
 	return Math.floor(min + Math.random() * (max - min));
@@ -29,50 +26,51 @@ function createSubdivRange(fromTo, minMax) {
 }
 
 const cols = createSubdivRange(
-	{ from: 0, to: bgWrapper.clientWidth },
+	{ from: 0, to: starsWrapper.clientWidth },
 	cellWidthRange
 );
 const rows = createSubdivRange(
-	{ from: 0, to: bgWrapper.clientHeight },
+	{ from: 0, to: starsWrapper.clientHeight },
 	cellHeightRange
 );
-
-for (let ci = 0; ci < cols.length; ci++) {
-	for (let ri = 0; ri < rows.length; ri++) {
-		const { position: x, size: width } = cols[ci];
-		const { position: y, size: height } = rows[ri];
-
-		const left = lerp(x, x + width, Math.random());
-		const top = lerp(y, y + height, Math.random());
-
-		const span = document.createElement("span");
-
-		span.classList.add("bg-item", `bg-item--${random(1, 38)}`);
-		span.style.width = `${width / 3}px`;
-		span.style.height = `${height / 3}px`;
-		span.style.top = `${top}px`;
-		span.style.left = `${left}px`;
-
-		if ((ri + 1) % 3 === 0) {
-			nodeList3.push(span);
-		} else if ((ri + 1) % 2 === 0) {
-			nodeList2.push(span);
-		} else {
-			nodeList1.push(span);
-		}
-	}
-}
 
 function appendBgBlockToWrapper(bgBlockNode, nodeList) {
 	bgBlockNode.append(...nodeList);
 
 	const clone = bgBlockNode.cloneNode(true);
-	clone.classList.add("bg-block--clone");
-	bgWrapper.append(clone);
+	clone.classList.add("stars-block--clone");
+
+	starsWrapper.append(clone);
 }
 
-appendBgBlockToWrapper(bgBlock1, nodeList1);
-appendBgBlockToWrapper(bgBlock2, nodeList2);
-appendBgBlockToWrapper(bgBlock3, nodeList3);
+function createStars(wrapperNode, sizeDivider = 6) {
+	const nodeList = [];
 
-bgWrapper.classList.add("active");
+	for (let ci = 0; ci < cols.length; ci++) {
+		for (let ri = 0; ri < rows.length; ri++) {
+			const { position: x, size: width } = cols[ci];
+			const { position: y, size: height } = rows[ri];
+
+			const left = lerp(x, x + width, Math.random());
+			const top = lerp(y, y + height, Math.random());
+
+			const span = document.createElement("span");
+
+			span.classList.add("stars-item", `stars-item--${random(1, 38)}`);
+			span.style.width = `${width / sizeDivider}px`;
+			span.style.height = `${height / sizeDivider}px`;
+			span.style.top = `${top}px`;
+			span.style.left = `${left}px`;
+
+			nodeList.push(span);
+		}
+	}
+
+	appendBgBlockToWrapper(wrapperNode, nodeList);
+}
+
+createStars(starsBlock1);
+createStars(starsBlock2, 4.5);
+createStars(starsBlock3, 3);
+
+starsWrapper.classList.add("active");
